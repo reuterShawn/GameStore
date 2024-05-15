e7310fe2589e47eb2128618ffdd2ff47f043fa1c
reuterShawn <reutershawn45@gmail.com>
2024-05-15 16:17:25 -0500

    Create model for table

---
+++ GameStore.frontend/Models/GameSummary.cs
@@ -0,0 +1,16 @@
+﻿namespace GameStore.frontend.Models;
+
+
+public class GameSummary
+{
+    public int Id {get; set;}
+
+    public required string? Name {get; set; }
+
+    public required string Genre {get; set;}
+
+    public decimal? Price {get; set;}
+
+    public DateOnly ReleaseDate {get; set;}
+
+}
